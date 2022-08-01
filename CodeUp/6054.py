9a3109d64ec25011d52b295bd99aba9a3418c406
Hyeon0630 <89723637+Hyeon0630@users.noreply.github.com>
2022-08-01 23:25:15 +0900

    8/1

---
+++ CodeUp/6054.py
@@ -0,0 +1,8 @@
+# and &
+# or |
+# xor ^
+
+a, b = input().split()
+a = bool(int(a))
+b = bool(int(b))
+print(a & b)
---
+++ CodeUp/6059.py
@@ -0,0 +1,9 @@
+# ~ (bitwise not) 
+# & (bitwise and)
+# | (bitwise or)
+# ^ (bitwise xor)
+# << (bitwise left shift)
+# >> (bitwise right shift)
+
+a = int(input())
+print(~a)
