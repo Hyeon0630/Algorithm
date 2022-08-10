53a89226965c3ef64fa43b8dffccf506b3f13e51
Hyeon0630 <89723637+Hyeon0630@users.noreply.github.com>
2022-08-10 22:56:54 +0900

    8/10

--- CodeUp/6097.py
+++ CodeUp/6097.py
@@ -21,22 +21,3 @@ for i in range(h) :
     for j in range(w) : 
         print(array[i][j], end = ' ')
     print()
-
-# input =[]
-# for i in range(n) :
-#     input.append([]) 
-#     for j in range(4) :
-#         input[i].append(input().split())
-#         # 0 길이 1 방향 2 x 3 y
-
-# for i in range(n) :
-#     for j in range(4) :
-#         input[i][j] = int(input[i][j])
-
-# for i in range(n) :
-#     if input[i][1] == 0 : 
-#         for j in range(input[i][3]-1, input[i][3]-1+input[i][0], 1) : 
-#             array[input[i][2]-1][j] = 1
-#     elif input[i][1] == 1 :
-#         for j in range(input[i][2]-1, input[i][2]-1+input[i][0], 1) : 
-#             array[j][input[i][3]-1] = 1        
