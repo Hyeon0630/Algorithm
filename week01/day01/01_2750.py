71d4d026a2d2e7341f447c50f96b8fd3ca8c25cf
Hyeon0630 <sonabykwon@gmail.com>
2024-01-10 21:53:12 +0900

    Connection Test 4

--- week01/day01/01_2750.py
+++ week01/day01/01_2750.py
@@ -1,4 +1,11 @@
-# 시간 복잡도 계산
-# input N을 받는다
-# N바퀴 돌면서 수를 리스트에 입력
-# 정렬 알고리즘으로 오름차순 정렬
+# 시간 복잡도 계산 - 정렬 알고리즘 결정
+# input N을 저장
+# N번 돌면서 input을 리스트 n_list에 입력
+# 정렬 알고리즘으로 오름차순 정렬 y_list
+
+N = input()
+n_list = [N]
+
+for i in range(N):
+ n_list[i] = input()
+
