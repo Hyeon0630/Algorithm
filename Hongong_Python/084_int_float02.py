6cb1a335aaa420ce7afc5672f6bc09d8c0b33218
Hyeon0630 <sonabykwon@gmail.com>
2024-01-20 15:33:56 +0900

    2024/01/20

---
+++ Hongong_Python/084_int_float02.py
@@ -0,0 +1,7 @@
+input_a = float(input("첫 번째 숫자: "))
+input_b = float(input("두 번째 숫자: "))
+
+print("+: ", input_a + input_b)
+print("-: ", input_a - input_b)
+print("*: ", input_a * input_b)
+print("/: ", input_a / input_b)
