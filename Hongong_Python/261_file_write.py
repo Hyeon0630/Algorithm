aa2aa041d6924785122fa4f430e4956c710a7559
Hyeon0630 <sonabykwon@gmail.com>
2024-01-29 02:40:00 +0900

    2024/01/29

--- Hongong_Python/261_file_write.py
+++ Hongong_Python/261_file_write.py
@@ -1,1 +1,10 @@
 import random
+
+hanguls = list("가나다라마바사아자차카타파하")
+
+with open("261_file_write_basic.txt", "w") as file:
+    for i in range(100):
+        name = random.choice(hanguls) + random.choice(hanguls)
+        weight = random.randrange(40,100)
+        height = random.randrange(140, 200)
+        file.write("{}, {}, {}\n".format(name, weight, height))
