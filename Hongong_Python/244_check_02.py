60daf346c8e6df8d006251af375d86f51bb4eeb5
Hyeon0630 <sonabykwon@gmail.com>
2024-01-28 23:42:20 +0900

    2024/01/28

--- Hongong_Python/244_check_02.py
+++ Hongong_Python/244_check_02.py
@@ -4,23 +4,28 @@ p = 100
 
 memo = {}
 
-def Q(no, yes):
+def Q(no, yes): # 98 2 10 90
+    count = 0
     key = str([no, yes])
 
     if key in memo:
         return memo[key]
     if no < 0:
-        return 0
+        return count
     if no == 0:
-        return 1
+        return count
     
     # 재귀 처리
-        count = 0
-        for i in range(yes, MAX + 1):
-            count += Q(no - 1, i)
+        for i in range(yes, no + 1): # i = 2~100
+            if no > MAX:
+                yes += i # 4
+                no -= i # 96
+                Q(no, yes)
+            else:
+
     # 메모화 처리
         memo[key] = count
     # 종료
         return count
     
-print(Q(p, MIN))
+print(Q(p-MIN, MIN))
--- Hongong_Python/244_check_02_my.py
+++ Hongong_Python/244_check_02_my.py
@@ -1,14 +1,37 @@
-def sit(rest, sitted) : 
-    count = 0
-    if rest == 2:
-        count += 1
-        return count
-    elif rest == 1:
-        return count
-    elif 2 < rest <= 100:
-        for i in (2, 11):
-            rest -= i
-            sitted += i
-            return sit(rest, sitted)
+# 10 * 10 => 1
+# 10을 분할
+# (8,2), (7,3), (6,4), (6,2,2), (5,5), (5,3,2), (4,4,2), (4,3,3), (4,2,2,2), (3,3,2,2), (2,2,2,2,2)
+# 11가지 + 1
+# 12H10 = 21C10
+# nCr = n!/r!(n-r)!
 
-print(sit(4, 2))
+MIN = 2
+
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+    
+def ncr(n, r):
+    a = factorial(n)
+    b = factorial(r)*factorial(n-r)
+    result = int(a/b)
+    return result
+
+def count_n(n):
+    count = 1
+    for i in range(2, n//2 + 1): # 10 - 2, 3, 4, 5 // 9 - 2, 3, 4
+        case = [n-i, i]
+        count += 1
+        for j in case:
+            if j != 2:
+                count += 1
+                count_n(j)
+    return count
+
+# 10 * 10 이므로
+n = count_n(10)
+r = 10
+result = int(ncr(n+r-1, r))
+print(result)
