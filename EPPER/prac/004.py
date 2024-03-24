179a0dc4df31055a2f63525be5c7978347d1eae0
Hyeon0630 <sonabykwon@gmail.com>
2024-03-24 11:21:27 +0900

    2024.03.24

--- EPPER/prac/004.py
+++ EPPER/prac/004.py
@@ -4,43 +4,40 @@ input = sys.stdin.readline
 s = list(input())
 s.pop()
 
-# 일단 window 길이 8>4~2, 9 -> 9//2~2
+def cntl(arr, unit):
+    nlength = len(unit)
+    unitarr = ""
+    cnt = 1
+
+    lstart = 0
+    rstart = unit
+
+    while rstart <= len(unit):
+        comparr = arr[lstart:rstart]
+
+        if unitarr == comparr:
+            cnt += 1
+        else:
+            if cnt > 1:
+                nlength += len(str(cnt))
+                nlength -= (cnt - 1) * unit
+            unitarr = comparr
+            cnt = 1
+
+        lstart += unit
+        rstart += unit
+
+        if cnt > 1:
+                nlength += len(str(cnt))
+                nlength -= (cnt - 1) * unit
+
+    return nlength
 
 def solution(s):
-    result = []
-    window = len(s)//2
-    while(window >= 2):
-        # 9 이면 4, 0~8 5678 len(s) = 9 - 4
-        # window가 4이면 for i in range(0, len(s), 1)
-        for i in range(0, len(s)-window+1):
-            check = s[i:i+window]
-            uncheck = s[i+window:]
-            cnt = 1
-            while(1):
-                start = i+window
-                end = i+2*window
-                if check in s[start:end]:
-                    cnt += 1
-                    start += window
-                    end += window
-                else:
-                    break
-            if cnt >= 2:
-                result.append(cnt)
-                result.append(check)
-                result.append(uncheck)
-                break
-                # 재귀함수
-            else:
-                window -= 1
-
-    # start = 0, end = len(s) // 2
-    # 인덱스 start, end를 두고 s[start:end] == s[end:2end-start]
-    # 맞으면 result.append(2)
-    # result.append(s[start:end])
-    # 없으면 end -= 1
-    # start == end이면 start += 1, end = len(s)-start // 2
+    ans = len(s)
+    for i in range(1, len(s)//2+1):
+        ans = min(ans, cntl(s, i))
     
-    return len(result)
+    return ans
 
 print(solution(s))
--- EPPER/prac/007_10971.py
+++ EPPER/prac/007_10971.py
@@ -9,30 +9,28 @@ ans = INF
 visited = []
 matrix = []
 
-def backt(n, cnt, curcity, cost):
+def backt(n, cnt, curc, cost):
     global ans, visited, matrix
     if cost >= ans:
         return
+    
     if cnt == n:
-        if matrix[curcity][0] != 0:
-            ans = min(ans, cost + matrix[curcity][0])
+        if matrix[curc][0] != 0:
+            ans = min(ans, cost + matrix[curc][0])
         return
-    for i in range(0, n):
-        if matrix[curcity][i] and visited[i] == 0:
+    
+    for i in range(n):
+        if matrix[curc][i] and visited[i] == False:
             visited[i] = True
-            backt(n, cnt+1, i, cost+matrix[curcity][i])
+            backt(n, cnt + 1, i, cost + matrix[curc][i])
             visited[i] = False
-
 
 def solution(n, cost):
     global ans, visited, matrix
-
-    visited = [False]*n
+    visited = [False] * n
     visited[0] = True
     matrix = cost
-    
-    backt(n, 1, 0, 0)
-
+    backt(n, 1, 0, 0)    
     return ans
 
 print(solution(N, W))
