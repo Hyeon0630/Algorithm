07a68da2a3e913040cab8fa5064c0fdf7c100e54
Hyeon0630 <sonabykwon@gmail.com>
2024-03-24 11:39:48 +0900

    2024.03.24

--- EPPER/prac/004.py
+++ EPPER/prac/004.py
@@ -5,39 +5,39 @@ s = list(input())
 s.pop()
 
 def cntl(arr, unit):
-    nlength = len(unit)
-    unitarr = ""
+    length = len(arr)
+    nlength = length
     cnt = 1
+    p = ""
 
-    lstart = 0
-    rstart = unit
+    l = 0
+    r = unit
 
-    while rstart <= len(unit):
-        comparr = arr[lstart:rstart]
-
-        if unitarr == comparr:
+    while r <= length:
+        c = arr[l:r]
+        if p == c:
             cnt += 1
         else:
             if cnt > 1:
                 nlength += len(str(cnt))
-                nlength -= (cnt - 1) * unit
-            unitarr = comparr
+                nlength -= unit * (cnt - 1)
             cnt = 1
+            p = c
 
-        lstart += unit
-        rstart += unit
+        l += unit
+        r += unit
 
-        if cnt > 1:
-                nlength += len(str(cnt))
-                nlength -= (cnt - 1) * unit
-
+    if cnt > 1:
+        nlength += len(str(cnt))
+        nlength -= unit * (cnt - 1)
+        
     return nlength
 
+
 def solution(s):
-    ans = len(s)
-    for i in range(1, len(s)//2+1):
-        ans = min(ans, cntl(s, i))
-    
-    return ans
+    answer = len(s)
+    for i in range(1, len(s) // 2 + 1):        
+        answer = min(answer, cntl(s, i))
+    return answer
 
 print(solution(s))
